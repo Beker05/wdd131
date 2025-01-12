4943ce6bd0e74a757d472802557c6b7996ee7643
Beker Cubillas <neriobeker_05@hotmail.com>
2025-01-11 19:41:48 -0500

    modified:   scripts/getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,6 @@
-// Obtener el año actual para los derechos de autor
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
-// Obtener la fecha de la última modificación del documento
+
 const lastModified = document.lastModified;
 document.getElementById("modifiedDate").textContent = lastModified;
