ba0b96fda0902e26acc38941b7a3ec086b22444c
Beker Cubillas <155118544+Beker05@users.noreply.github.com>
2026-05-09 18:26:37 -0500

    Add files via upload

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+const year = document.querySelector("#currentyear");
+
+const today = new Date();
+
+year.innerHTML = today.getFullYear();
+
+const lastModified = document.querySelector("#lastModified");
+
+lastModified.innerHTML = `Last Modified: ${document.lastModified}`;
